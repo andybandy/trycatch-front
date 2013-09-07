77ce123c3882450d81d493fc142411718c1cca4a
Andrei Shchatko <andrei.shchatko@gmail.com>
2013-09-07 14:39:20 +0400

    [HACK!!!] script for building and copying ember app to trycatch-server.
    Works only if you put trycatch-front to the same dir with trycatch-server.
    Be carefull

---
+++ build.sh
@@ -0,0 +1,4 @@
+brunch build
+cp public/index.html ../trycatch-server/views/app.ejs
+cp public/stylesheets/* ../trycatch-server/public/stylesheets/
+cp public/javascripts/* ../trycatch-server/public/javascripts/
